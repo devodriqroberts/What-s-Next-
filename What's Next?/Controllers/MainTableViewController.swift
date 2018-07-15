1aa138e36febc59feec2573c5f43cf64725301ac
DevoRoberts <devorob26@gmail.com>
2018-07-14 22:02:09 -0400

    Add checkmark functionality to list table view.

---
+++ What's Next?/Controllers/MainTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MainTableViewController.swift
+//  What's Next?
+//
+//  Created by Devodriq Roberts on 7/14/18.
+//  Copyright © 2018 Devodriq Roberts. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Study Firebase", "Study Alamofire"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Add checkmark functionality to list table view
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+   
+
+   
+
+}
